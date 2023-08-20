9dfa18244eb9a030a973042d9b4b82a7aebb7941
George Townley <george@knowlesavenue.com>
2023-08-20 11:09:52 +0100

    first attempt at comparison table of two players

--- src/components/PlayerInfo.js
+++ src/components/PlayerInfo.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const skillNames = [
+const SKILLS = [
   "Overall",
   "Attack",
   "Defence",
@@ -27,57 +27,84 @@ const skillNames = [
   "Construction",
 ];
 
-async function fetchPlayerData() {
-  const url = "/m=hiscore_oldschool_seasonal/index_lite.ws?player=Unholy304";
+const fetchPlayerData = async (playerName) => {
+  const url = `/m=hiscore_oldschool_seasonal/index_lite.ws?player=${encodeURIComponent(
+    playerName
+  )}`;
 
-  const response = await fetch(url);
-  const data = await response.text();
-  return data;
-}
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch player data for ${playerName}`);
+    }
+    const data = await response.text();
+    const parsedData = data.split("\n").map((skill) => skill.split(","));
+    return parsedData;
+  } catch (error) {
+    console.error("Error fetching player data:", error.message);
+    return null;
+  }
+};
 
-const PlayerInfo = () => {
-  const [playerInfo, setPlayerInfo] = useState([]);
-  const [error, setError] = useState(null);
+const PlayerInfo = ({ player1Name, player2Name }) => {
+  const [player1Stats, setPlayer1Stats] = useState([]);
+  const [player2Stats, setPlayer2Stats] = useState([]);
 
   useEffect(() => {
-    async function loadData() {
-      try {
-        const playerData = await fetchPlayerData();
-        const playerInfoArray = playerData.split("\n");
-        const formattedData = playerInfoArray
-          .slice(0, skillNames.length)
-          .map((info, index) => {
-            const [rank, level, experience] = info.split(",");
-            return {
-              skill: skillNames[index],
-              level: level,
-              experience: experience,
-            };
-          });
-        setPlayerInfo(formattedData);
-      } catch (err) {
-        setError(err.message);
-      }
+    async function fetchData() {
+      const data1 = await fetchPlayerData(player1Name);
+      const data2 = await fetchPlayerData(player2Name);
+      setPlayer1Stats(data1);
+      setPlayer2Stats(data2);
     }
+    fetchData();
+  }, [player1Name, player2Name]);
 
-    loadData();
-  }, []);
-
+  if (
+    !player1Stats ||
+    !player2Stats ||
+    player1Stats.length !== SKILLS.length ||
+    player2Stats.length !== SKILLS.length
+  ) {
+    return <div>Loading...</div>;
+  }
   return (
-    <div>
-      <h2>Player Stats:</h2>
-      {error ? (
-        <p>Error fetching player data: {error}</p>
-      ) : (
-        <ul>
-          {playerInfo.map((info, index) => (
-            <li key={index}>
-              {info.skill}: Level {info.level} ({info.experience}exp)
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>{player1Name}</th>
+          <th>Comparison</th>
+          <th>{player2Name}</th>
+        </tr>
+      </thead>
+      <tbody>
+        {SKILLS.map((skill, index) => {
+          const [player1Exp] = player1Stats[index];
+          const [player2Exp] = player2Stats[index];
+
+          let comparison;
+          if (player1Exp > player2Exp) {
+            comparison = "<";
+          } else if (player1Exp < player2Exp) {
+            comparison = ">";
+          } else {
+            comparison = "=";
+          }
+
+          return (
+            <tr key={index}>
+              <td>
+                {skill}: {player1Stats[index][1]} ({player1Exp} exp)
+              </td>
+              <td>{comparison}</td>
+              <td>
+                {skill}: {player2Stats[index][1]} ({player2Exp} exp)
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 };
 
